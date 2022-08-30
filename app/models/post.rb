32512d411a0627527c24316142613f9f6623788c
Kelly Duran <kaduran16@gmail.com>
2022-08-30 13:13:37 -0400

    commit tree a695e570f061ddbdafaed5b551fbc62288a9004e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,34 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length:  { maximum: 250 }
+    validates :category, inclusion: {
+        in: ['Fiction', 'Non-Fiction']
+      }
+    # validate :title_is_clickbait
+
+    # def title_is_clickbait(title)
+    #     must_include= ["Won't Believe", "Secret", "Top \d", "Guess"]
+    #     must_include.any?{|t| title.downcase.includes?(t.downcase)}
+    #     if title.valid? 
+    #         render json: post, status: :created
+    #     else
+    #         errors.add(:title, "Must include more clickbait-y words like Won't Believe, Secret, or Guess")
+    #     end
+    # end
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
 end
+
